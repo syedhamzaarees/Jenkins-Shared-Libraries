20595240faf0c11519d16a922d69190dec29b630
syedhamzaarees <syedahmedarees@gmail.com>
2025-10-22 19:06:56 +0500

    Update buildFlask.groovy

--- vars/buildFlask.groovy
+++ vars/buildFlask.groovy
@@ -1,1 +1,10 @@
 
+def call(Map config = [:]) {
+    stage('Build Flask App') {
+        echo "Step 2: Building the Flask Docker image..."
+        sh """
+            docker build -t ${config.imageName ?: 'flask-app:latest'} ./flask_app
+        """
+        echo "✅ Flask image built successfully: ${config.imageName ?: 'flask-app:latest'}"
+    }
+}
