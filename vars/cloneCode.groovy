a8ad4ece5c53fa7cf33b2a7097742717f6aaf0b4
syedhamzaarees <syedahmedarees@gmail.com>
2025-10-22 19:04:07 +0500

    Update cloneCode.groovy

--- vars/cloneCode.groovy
+++ vars/cloneCode.groovy
@@ -1,1 +1,9 @@
 
+def call(Map config = [:]) {
+    stage('Clone Code') {
+        echo "Step 1: Cloning the repository..."
+        git url: config.repo ?: 'https://github.com/syedhamzaarees/Flask-based-web-application-with-Nginx-reverse-proxy-and-MySQL-database-fully-containerized-using-D.git',
+            branch: config.branch ?: 'main'
+        echo "✅ Code cloned successfully from ${config.repo}."
+    }
+}
