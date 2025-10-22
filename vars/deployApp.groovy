523101c7335af6bb7185415b7816f3a277f6bd5d
syedhamzaarees <syedahmedarees@gmail.com>
2025-10-22 19:09:34 +0500

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,1 +1,10 @@
 
+def call(Map config = [:]) {
+    stage('Deploy Application') {
+        echo "Step 5: Deploying application using Docker Compose..."
+        sh """
+            docker-compose up -d --build
+        """
+        echo "✅ Application deployed successfully!"
+    }
+}
