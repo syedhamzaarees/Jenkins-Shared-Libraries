38d7f83e1866c7b83efba152f52f034fffa873e5
syedhamzaarees <syedahmedarees@gmail.com>
2025-10-22 19:07:55 +0500

    Update buildNginx.groovy

--- vars/buildNginx.groovy
+++ vars/buildNginx.groovy
@@ -1,1 +1,10 @@
 
+def call(Map config = [:]) {
+    stage('Build Nginx App') {
+        echo "Step 3: Building the Nginx Docker image..."
+        sh """
+            docker build -t ${config.imageName ?: 'nginx-app:latest'} ./nginx
+        """
+        echo "✅ Nginx image built successfully: ${config.imageName ?: 'nginx-app:latest'}"
+    }
+}
